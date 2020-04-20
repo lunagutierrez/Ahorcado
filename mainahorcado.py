a411febc5445cf873338cabe4259925e8164f032
anamarigarzon <63021584+anamarigarzon@users.noreply.github.com>
2020-04-20 18:10:35 -0500

    Update mainahorcado.py

--- mainahorcado.py
+++ mainahorcado.py
@@ -6,23 +6,26 @@ Created on Wed Apr  1 18:21:29 2020
 """
 
 import ahorcadoind
+import ahorcadomult
 
 def juego():
-    print("\nWelcome to 'Hangman'")
-    
+    print("\nBienvenido al Juego de Ahorcado")
+    for i in range(4):
+        print("")
+        
     while True:
-        try:
-            version = int(input("Individual [1] o Multiplayer? [2]\n"))
+        version = int(input("Individual [1] o Multijugador? [2]  \n"))
+        if version == 1:
+            ahorcadoind.setup_ind()
+            break
+        elif version == 2:
+            ahorcadomult.setup_mult()
+            break
+        else:
+            print("Por favor, elige 1 o 2")
+        
+        
 
-            if version == 1:
-                ahorcadoind()
-                break
-#            elif version == 2:
-#                ahorcadomult()
-                break
-            else:
-                print("\nPlease pick '1' or '2'")  
-        except:
-            print("Please pick '1' or '2'")
-
+#BEGINNING-OF-EXECUTION
 juego()
+#END-OF-EXECUTION
