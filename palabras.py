6ebc47f30c7f66b74346de64476e8ccabd7adb38
anamarigarzon <63021584+anamarigarzon@users.noreply.github.com>
2020-04-20 18:06:02 -0500

    Update palabras.py

--- palabras.py
+++ palabras.py
@@ -4,15 +4,17 @@ Created on Wed Apr  1 17:17:34 2020
 
 @author: lunag
 """
+
 import sys
 
-# definición de prueba que nos permite saber si cada palabra de cada lista tiene la longitud correspondiente
+#Función que nos permite probar que cada palabra tenga la longitud asignada
 def comprobar(lenw,palx):
     for i in palx:
         if len(i)==lenw:
             return True
         else:
             return False
+            break
         
 def test(pasa):
     lnum=sys._getframe(1).f_lineno
@@ -21,8 +23,6 @@ def test(pasa):
     else:
         msg="Prueba en la línea {0} FALLÓ.".format(lnum)
     print(msg)
-    
- #Nos permite comprobar la definición de prueba
     
 def testsuite():
     test(comprobar(3,pal3)==True)
@@ -33,7 +33,8 @@ def testsuite():
     test(comprobar(8,pal8)==True)
     test(comprobar(9,pal9)==True)
     test(comprobar(10,pal10)==True)
-    
+
+#Listas de palabras por longitud
 
 pal3=["sea", "uno", "pie", "ojo", "ajo", "oca", "sed", "tul", "dos", "tos", "luz", "voz", "tez", "uva", "pan", "mar", "oso", "ola", "sol", "pez", "col", "sal" , "paz", "cal", "rio", "res", "ron", "lio", "mes", "pis", "par", "tio", "mio", "pua", "mes", "sur", "bol", "bar", "ala", "res", "can", "los", "via", "asa", "feo"]
 pal4=["masa", "como", "mopa", "dado", "gafa", "mesa", "copo", "moda", "dedo", "gala", "misa", "cono", "mofa", "dudo", "gama", "musa", "coco", "lona", "gata", "codo", "moja", "gasa", "coso", "mora", "garra", "coto", "mota", "gana", "cojo", "loza", "paso", "lana", "paja", "rana", "tala", "peso", "lama", "mala", "rama", "tapa", "puso", "lava", "mapa", "raza", "mata"]
@@ -43,17 +44,19 @@ pal7=["hermosa","estudio","encanto","palabra","carisma","zapateo","caballo","beb
 pal8=["internet","labiales","sociales","biologia","viviparo","longitud","mariposa","nacional","colombia","creativo","embarazo","orquesta","manicura","vibrante","xilofono","incendio","pelicula","finanzas","economia","libertad","gaviotas","cerebelo","fraccion","infinito","libelula","amazonas","ciencias","subsidio","sintomas","degustar","telefono","almohada","hipnosis","misterio","suspenso","analisis","perfecto","claustro","estudiar","personas","pancreas","deportes","ahorcado","piramide","multitud"]
 pal9=["corolario","diafragma","antebrazo","sarampion","zanahoria","higienico","cuarentena","suculento","australia","enamorado","occidente","relampago","feminismo","eufemismo","prematuro","filosofia","prudencia","mitologia","dieciseis","provincia","enfermero","funciones","fortaleza","mapamundi","dimension","animales","fractales","dieciocho","exactitud","simpatico","inquietud","cincuenta","serenidad","paciencia","inclusion","depresion","romantico","miercoles","crescendo","confesion","barcelona","mordedura","audifonos","bendicion","dramatico"]
 pal10=["videojuego","matematica","computador","asociacion","hemisferio","granadilla","hipotalamo","arquitecto","calendario","tecnologia","hipopotamo","crucigrama","aritmetica","matrimonio","salchichon","transexual","psicologia","diversidad","tartamudeo","comentario","abecedario","superheroe","vacacional","uniformado","uzbekistan","jamaiquino","judicatura","obligacion","oficinista","defectuoso","cumpleaños","disciplina","plastilina","natureleza","mendicidad","zarigueyas","adaptacion","campesinos","lentejuela","despiadado","etnografia","transmitir","crepusculo","hospitales","sarpullido"]
-
+ 
 #BEGINNING-OF-EXECUTION
-""" 
-# En las pruebas, imprimimos la longitud de las listas, para asegurarnos que por cada longitud hubiesen 45 palabras
-print (len(pal3))
+
+#Imprime el número de palabras de cada lista, nos permitió asegurarnos de que para cada longitud habían 45 palabras
+"""print (len(pal3))
 print (len(pal4))
 print (len(pal5))
 print (len(pal6))
 print (len(pal7))
 print (len(pal8))
 print (len(pal9))
-print (len(pal10))
-testsuite()
-#END-OF-EXECUTION """
+print (len(pal10))"""
+
+#Hace la prueba definida al principio
+"""testsuite()"""
+#END-OF-EXECUTION
